73df48d90d317b07d8bde7043013be7f2d305385
INFOBEANS\Akshay.Patidar <faraz.ahmad@infobeans.com>
2018-04-26 15:47:26 +0530

    1 Branch

--- GITProject/app/src/main/java/com/example/akshaypatidar/gitproject/MainActivity.java
+++ GITProject/app/src/main/java/com/example/akshaypatidar/gitproject/MainActivity.java
@@ -2,12 +2,33 @@ package com.example.akshaypatidar.gitproject;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements OnClickListener {
+
+    private TextView textViewCenterText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textViewCenterText = (TextView) findViewById(R.id.textViewCenterText);
+        textViewCenterText.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.textViewCenterText:
+                Toast.makeText(MainActivity.this, "Hello ! This is branch code.", Toast.LENGTH_LONG).show();
+                break;
+
+            default:
+                break;
+        }
     }
 }
