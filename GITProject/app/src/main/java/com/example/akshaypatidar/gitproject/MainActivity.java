0cfff95695a46673b9a9b4e3e8b4a48f2d16a359
INFOBEANS\Akshay.Patidar <faraz.ahmad@infobeans.com>
2018-04-26 15:09:02 +0530

    1 Master

--- GITProject/app/src/main/java/com/example/akshaypatidar/gitproject/MainActivity.java
+++ GITProject/app/src/main/java/com/example/akshaypatidar/gitproject/MainActivity.java
@@ -2,6 +2,10 @@ package com.example.akshaypatidar.gitproject;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView textViewCenterText=(TextView)findViewById(R.id.textViewCenterText);
+        textViewCenterText.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                performClickForMaster();
+            }
+        });
     }
+
+    public void performClickForMaster()
+    {
+        Toast.makeText(MainActivity.this,"You are in Master",Toast.LENGTH_LONG).show();
+    }
+
 }
+
